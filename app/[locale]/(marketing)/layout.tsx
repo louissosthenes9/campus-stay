77d8cfde95cf75831f47285893df7345cbff513f
louissosthenes9 <louissosthenes9@gmail.com>
2025-04-12 00:29:00 +0300

    updated  ui

--- app/[locale]/(marketing)/layout.tsx
+++ app/[locale]/(marketing)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "../../global.css";
 
 // Initialize the Inter font
 const inter = Inter({ 
